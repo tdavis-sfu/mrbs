8313094068a172cfce114ff22f99a47ea4d4e5af
Trevor <tdavis.sfu@gmail.com>
2021-09-02 05:06:58 +0000

    cas

--- config.inc.php
+++ config.inc.php
@@ -178,7 +178,7 @@ $auth['cas']['context'] = '/cas';  // Context of the CAS Server
 // For quick testing you can disable SSL validation of the CAS server.
 // THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
 // VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-$auth['cas']['no_server_validation'] = false;
+$auth['cas']['no_server_validation'] = true;
 
 // Filtering by attribute
 // The next two settings allow you to use CAS attributes to require that a user must have certain
